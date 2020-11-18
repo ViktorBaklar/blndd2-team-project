3cf02298c006c1d5cab419f880c396429ef517d7
RinaOcean <antyfifa@gmail.com>
2020-11-18 17:02:41 +0200

    Updates

--- registration.e58f904c.js
+++ registration.e58f904c.js
@@ -154,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59211" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64434" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
